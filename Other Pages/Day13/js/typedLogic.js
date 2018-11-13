ea86e48364aeb1e6a2c09127e982141e75036e0d
Kevin Saechew <kevin_saechew@brown.edu>
2018-11-13 17:39:30 +0800

    Animation CSS

---
+++ Other Pages/Day13/js/typedLogic.js
@@ -0,0 +1,13 @@
+function initType() {
+
+var typed = new Typed(document.getElementsByClassName('typed')[0], {
+    strings: ["Pikachu!!!!", "Squirtle!!!", "Not Bulbasaur...", "Charmander!!", "AND MEOWTH!!!"],
+    typeSpeed: 80,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+
+
+}
+  
+
